fc1852dcb573a01d995ab9a5c11a222ef5e841cb
hoorasadat <mirsaanei@gmail.com>
2023-08-29 15:37:57 +0000

    Add project files.

---
+++ Inventory.aspx.cs
@@ -0,0 +1,19 @@
+﻿using InventoryData_;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace InventoryGui
+{
+    public partial class Inventory : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            List<Supplier> suppliers = SupplierManager.GetSuppliers();
+            gvInventory.DataSource = suppliers;
+        }
+    }
+}
